dd02b173ef5fdb77846bd26ee328fc41f7e02364
andela-pijege <precious.ijege@andela.com>
2017-11-25 14:04:09 +0100

    add run test function

--- manage.py
+++ manage.py
@@ -16,6 +16,14 @@ def make_shell_context():
     return dict(app=app)
 
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
