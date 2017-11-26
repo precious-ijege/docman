7f278bde2edf1770956f403b16e2f5713108eec9
andela-pijege <precious.ijege@andela.com>
2017-11-26 13:42:10 +0100

    add seed data command

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from app import create_app
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 from app import db
+from app.models.models import Roles
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
@@ -14,6 +15,15 @@ manager.add_command('db', MigrateCommand)
 
 def make_shell_context():
     return dict(app=app)
+
+
+@manager.command
+def seed():
+    " Seeds the database with predefined roles"
+    admin_role = Roles(title="admin")
+    regular_role = Roles(title="regular")
+    db.session.add_all([admin_role, regular_role])
+    db.session.commit()
 
 
 @manager.command
